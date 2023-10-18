bb457a1e101cbec3241a92e2c4b8a65198fa4b67
Jeremie-mengi <jeremiemengi248@gmail.com>
2023-10-18 20:26:45 +0200

    Second

--- Exo.js
+++ Exo.js
@@ -1,1 +1,104 @@
-let 
+// // 1
+// let firstname = "lata"
+// // 2
+// let  x = 'Geta'
+// // 3
+// let flower = 'rose'
+// let tree =  'maple'
+// // 4
+// let x = 'Tic';
+// x = 'Tac';
+// x = 'Toe'
+// // 5
+// let x = 'Laurel';
+// let y = 'Hardy';
+// let z = y;
+// y = x;
+// x = z;
+// // 6
+// function hello (){
+//     return 'hello word'
+// }
+// // 7
+// function a (){
+//     return 'a'
+// }
+
+// function b (){
+//     return 'b'
+// }
+// // 8
+// function greet(){
+//     return 'Haydo!'
+// }
+// let salutation = greet();
+// // 9
+// function hello() {
+//     return 'Hi!';
+//   }
+  
+//   let x = hello();
+
+// // 10
+// function echo(para){
+// return para 
+// }
+// echo(CO2)
+// // 11
+// function reply(phrase) {
+//     return phrase;
+//   }
+  
+//   let x = reply('How do you do?');
+
+// //   12
+// function greet(name){
+//     return 'hello'+ name
+
+// }
+// console.log(greet('MENGI'))
+// // // 13
+// // Que vaut X 
+
+// function whereIs(name) {
+//   return 'Where is ' + name + '?';
+// }
+
+// let x = whereIs('Jacky');
+
+// // 14  
+
+// function hi(name) {
+//   return 'Hi ' + name + '!';
+// }
+
+// let h1 = hi('Selva');
+// let h2 = hi('Pola');
+// let x = h1 + ' ' + h2
+
+// // 15
+// function shout(name){
+// return name+name
+// }
+// console.log(shout('mengi'))
+// // // 16
+// // Que vaut x
+// function double(name) {
+//   return name + ' and ' + name;
+// }
+
+// let x = double('Roy');
+// // 17
+// function length(sun){
+//     return sun.length
+// }
+// console.log(length('JEREMIE'))
+// 18
+function toCase(str){
+return str.toLowerCase() + '-' + str.toUpperCase()
+}
+console.log(toCase("MENGI"))
+// 19
+function shortcut(name1,name2){
+return name1+name2.substr()
+}
